d2a700236d0811ba62473df60904601ffaebd05a
Sumit Gaikwad <sumit.gaikwad@prorigosoftware.com>
2025-10-24 17:19:59 +0530

    added validations

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,10 +1,19 @@
-﻿namespace WebApplicationPractice.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationPractice.Models
 {
     public class Employee
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Name cannot be empty")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Email cannot be empty")]
+        [EmailAddress(ErrorMessage = "NOT A VALID EMAIL")]
+        public string? Email { get; set; }
+
+        [Range(9000000000, 9999999999, ErrorMessage = "Only Maharashtra numbers are required")]
         public double Phone { get; set; }
     }
 }
