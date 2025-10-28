094f10bb0855cee0be647bec8fc82d48a19b0b4f
Sumit Gaikwad <sumit.gaikwad@prorigosoftware.com>
2025-10-28 18:00:04 +0530

    removed data annotation

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -6,14 +6,14 @@ namespace WebApplicationPractice.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Name cannot be empty")]
+        //[Required(ErrorMessage = "Name cannot be empty")]
         public string? Name { get; set; }
 
-        [Required(ErrorMessage = "Email cannot be empty")]
-        [EmailAddress(ErrorMessage = "NOT A VALID EMAIL")]
+        //[Required(ErrorMessage = "Email cannot be empty")]
+        //[EmailAddress(ErrorMessage = "NOT A VALID EMAIL")]
         public string? Email { get; set; }
 
-        [Range(9000000000, 9999999999, ErrorMessage = "Only Maharashtra numbers are required")]
+        //[Range(9000000000, 9999999999, ErrorMessage = "Only Maharashtra numbers are required")]
         public double Phone { get; set; }
 
         public long Salary {  get; set; }   
