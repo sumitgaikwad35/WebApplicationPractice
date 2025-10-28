ecdd48204550ce15eded58553720db7b57ee74cd
Sumit Gaikwad <sumit.gaikwad@prorigosoftware.com>
2025-10-28 17:59:03 +0530

    Added validators

---
+++ Validator/EmployeeValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using WebApplicationPractice.Models;
+
+namespace WebApplicationPractice.Validator
+{
+    public class EmployeeValidator : AbstractValidator<Employee>
+    {
+        public EmployeeValidator()
+        {
+            RuleFor(e => e.Email).NotEmpty().WithMessage("Email can not be empty").EmailAddress().WithMessage("Not a valid Email");
+            RuleFor(e => e.Name).NotEmpty().WithMessage("Name can not be empty").Length(2, 20).WithMessage("Name length should be between 2 and 20");
+            RuleFor(e => e.Salary).GreaterThan(0).WithMessage("Salary should more than 0");
+        }
+    }
+}
