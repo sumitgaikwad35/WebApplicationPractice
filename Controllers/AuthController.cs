3eae3e2a241f3ac45f64489ac7afed4e45900b39
Sumit Gaikwad <sumit.gaikwad@prorigosoftware.com>
2025-10-30 19:35:27 +0530

    Added Controller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly string _jwtKey = "YourSuperStrongSecretKeyWith256BitsSizeHere!!!";
+    private readonly string _issuer = "WebApplication";
+    private readonly string _audience = "your-users";
+
+    [HttpPost("login")]
+    public IActionResult Login([FromBody] LoginRequest request)
+    {
+        if (request.Username == "user" && request.Password == "password")
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, request.Username),
+                new Claim(ClaimTypes.Role, "User")
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _issuer,
+                audience: _audience,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds
+            );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return Ok(new { token = jwt });
+        }
+
+        return Unauthorized();
+    }
+}
