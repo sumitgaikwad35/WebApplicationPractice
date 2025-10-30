b38ce3014a04c5619f2e66ce1b3f1a738b52006d
Sumit Gaikwad <sumit.gaikwad@prorigosoftware.com>
2025-10-30 19:38:27 +0530

    added authentication and authorization

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Serilog;
 using StackExchange.Redis;
+using System.Text;
 using WebApplicationPractice.Data;
 using WebApplicationPractice.Services;
-
 
 namespace WebApplicationPractice
 {
@@ -20,24 +22,55 @@ namespace WebApplicationPractice
                 .Enrich.FromLogContext()
                 .CreateLogger();
 
-
             builder.Host.UseSerilog();
+
             builder.Services.AddControllers();
             builder.Services.AddFluentValidationAutoValidation();
             builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+
             builder.Services.AddStackExchangeRedisCache(options =>
             {
-                options.Configuration = "localhost:6379"; 
-                options.InstanceName = "EmployeeApp_";   
+                options.Configuration = "localhost:6379";
+                options.InstanceName = "EmployeeApp_";
             });
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-            builder.Services.AddScoped<IEmployeeServices , EmployeeDbServices>();
-            //builder.Services.AddSingleton<IEmployeeServices , EmployeeServices>();
-            builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddScoped<IEmployeeServices, EmployeeDbServices>();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            var jwtKey = "YourSuperStrongSecretKeyWith256BitsSizeHere!!!"; 
+            var validIssuer = "WebApplication";
+            var validAudience = "your-users";
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+
+                    ValidIssuer = validIssuer,         
+                    ValidAudience = validAudience,      
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+                };
+            });
+
+            builder.Services.AddAuthorization();
 
             var app = builder.Build();
 
+           
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -45,8 +78,12 @@ namespace WebApplicationPractice
             }
 
             app.UseHttpsRedirection();
+
+            app.UseAuthentication();
             app.UseAuthorization();
+
             app.MapControllers();
+
             app.Run();
         }
     }
